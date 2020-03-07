4031da2366299991afe79b33aaa97917b38a40b1
Kim Nguyen <kim314nguyen@gmail.com>
2020-03-07 14:44:34 -0800

    pritns message

--- our_first_thang_but_prettier.py
+++ our_first_thang_but_prettier.py
@@ -1,12 +1,13 @@
 from p5 import *
 import time
 import random
+from letters import letters
 
 square_size = 20
 padding = 10
 
 rows = 10
-cols = 20
+cols = 30
 lights = [[0 for i in range(rows)] for j in range(cols)]
 
 current_hue = 120
@@ -17,26 +18,23 @@ frames_to_change_hue = 10
 frame_num = 0
 frame_dir = 1
 
-letter_A = [
-    [0, 0, 1, 1, 1, 1, 1, 1],
-    [0, 1, 0, 0, 1, 0, 0, 0],
-    [1, 0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1]
-]
-
-
 def printChar(start_x, start_y, char):
     for char_x in range(len(char)):
         for char_y in range(len(char[0])):
             if char[char_x][char_y]:
-                lights[(start_x + char_x) %
-                       cols][(start_y + char_y) % rows] = 100
-
+                if (start_x + char_x) < cols and (start_x + char_x) >= 0:
+                    lights[(start_x + char_x) % cols][(start_y + char_y) % rows] = 100
 
 def printCharHorizontally(i, y, char):
-    printChar(cols - (i % cols), 1, letter_A)
-    # printChar(0, 0, letter_A)
+    printChar(cols - (i % cols), 1, char)
+
+def printMessage(start_x, start_y, message):
+    chars = list(message.upper())
+    for pos in (range(len(chars))):
+        printChar(start_x + pos * 6, start_y, letters[chars[pos]])
+
+def printMessageHorizontally(i, y, message):
+    printMessage(cols - (i % (len(message) * 10)), y, message)
 
 
 def getRandomPrettyColor():
@@ -64,14 +62,14 @@ def turnOn(start, count, frame_dir):
 
 def update(i, frame_dir):
     clearLights()
-    # printChar(1, 1, letter_A)
-    printCharHorizontally(i, 1, letter_A)
+    # printCharHorizontally(i, 1, letters["A"])
+    printMessageHorizontally(i, 1, "hello world")
     # turnOn(i, 3, frame_dir)
     time.sleep(0.05)
 
 
 def setup():
-    size(640, 360)
+    size(960, 360)
     no_stroke()
     color_mode('HSB', 360, 100, 100)
     background(0, 0, 0)
@@ -97,11 +95,11 @@ def draw():
     update(frame_num, frame_dir)
     frame_num += frame_dir
 
-    if random_uniform(100) < 3:
-        target_hue = random.randint(0, 360)
-        hue_shift_per_frame = abs(
-            current_hue - target_hue) / frames_to_change_hue
-        frame_dir *= -1
+    # if random_uniform(100) < 3:
+    #     target_hue = random.randint(0, 360)
+    #     hue_shift_per_frame = abs(
+    #         current_hue - target_hue) / frames_to_change_hue
+    #     frame_dir *= -1
 
     if (abs(current_hue - target_hue) < hue_shift_per_frame):
         current_hue = target_hue
